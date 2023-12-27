774485e27d0e242a521a8ce8a8ee7dc4d8efb8e2
MosheYitzhakov <126177360+MosheYitzhakov@users.noreply.github.com>
2023-12-27 16:24:07 +0200

    logo

--- front-end/src/components/header.js
+++ front-end/src/components/header.js
@@ -4,7 +4,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-
+// import Typography from '../../public/ss.png';
 export default function Header() {
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -16,7 +16,8 @@ export default function Header() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         booking-halls
                     </Typography>
-                    logo
+                    <img src="logoPages.png" alt="logoPagesB.png" width="100" height="100"/>
+
                 </Toolbar>
             </AppBar>
         </Box>

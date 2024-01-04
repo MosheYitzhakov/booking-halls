41bc7bb26923dfd0b1e3b5a8db49d6294bf00285
MosheYitzhakov <126177360+MosheYitzhakov@users.noreply.github.com>
2024-01-04 19:38:17 +0200

    dd

--- front-end/src/components/header.js
+++ front-end/src/components/header.js
@@ -16,7 +16,8 @@ export default function Header() {
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         booking-halls
                     </Typography>
-                    <img src="/logoPages.png" alt="logoPages.png" width="100" height="100"/>
+                    <img style={{borderRadius:"50%"}} src="/logoPages.png" alt="logoPages.png" width="100" height="100"/>
+            
 
                 </Toolbar>
             </AppBar>

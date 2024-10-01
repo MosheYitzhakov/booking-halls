5ed8ad9d219477c63208b169070c8477137cc7f6
shimon riss <shimy1045@gmail.com>
2024-10-01 12:37:22 +0300

    update API

--- front-end/src/API.js
+++ front-end/src/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 const url =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3335/api/"
-    : process.env.RENDER_EXTERNAL_URL + "/api/";
+    : process.env.REACT_APP_RENDER_EXTERNAL_URL + "/api/";
 console.log(url);
 const instance = axios.create({
   baseURL: url,
